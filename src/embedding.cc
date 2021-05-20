a5fb7881b786d6440b3cbaf5f627d7bead668e20
fjvxmap <83649602+fjvxmap@users.noreply.github.com>
2021-05-20 19:09:34 +0900

    Update embedding.cc

--- src/embedding.cc
+++ src/embedding.cc
@@ -6,34 +6,35 @@
 #include "embedding.h"
 
 Embedding::Embedding(const std::string& filename) {
-	std::ifstream fin(filename);
+    std::ifstream fin(filename);
 
-	if (!fin.is_open()) {
-		std::cout << "Embedding file " << filename << " not found!\n";
-		exit(EXIT_FAILURE);
-	}
+    if (!fin.is_open()) {
+        std::cout << "Embedding file " << filename << " not found!\n";
+        exit(EXIT_FAILURE);
+    }
 
-	char type;
-	size_t num_query_vertices;
+    char type;
+    size_t num_query_vertices;
+    size_t i = 0;
 
-	fin >> type >> num_query_vertices;
-	size_t i = 0;
-	embd_.clear();
-	while (fin >> type) {
-		if (type == 'a') {
-			Vertex v;
-			embd_.push_back(std::vector<Vertex>(num_query_vertices));
-			for (Vertex u = 0; u < num_query_vertices; u++) {
-				fin >> v;
-				embd_[i].push_back(v);
-			}
-			i++;
-		}
-	}
+    fin >> type >> num_query_vertices;
+    embd_.clear();
+    while (fin >> type) {
+        if (type == 'a') {
+            Vertex v;
+            embd_.push_back(std::vector<Vertex>(num_query_vertices));
+            embd_[i].clear();
+            for (Vertex u = 0; u < num_query_vertices; u++) {            
+                fin >> v;
+                embd_[i].push_back(v);
+            }
+        i++;
+        }
+    }
 
-	n = i;
+    n = i;
 
-	fin.close();
+    fin.close();
 }
 
 Embedding::~Embedding() {}
